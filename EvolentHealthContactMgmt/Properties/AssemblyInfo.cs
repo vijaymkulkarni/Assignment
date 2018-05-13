bc5c2ef8c3b5a99dde5cf8e7eef0f9174a469ec9
vijaymkulkarni <vijaymk_76@outlook.com>
2018-05-13 23:27:40 +0530

    Rename from EvolentHealthContactMgmt to ContactMgmt updated publishing details

--- EvolentHealthContactMgmt/Properties/AssemblyInfo.cs
+++ EvolentHealthContactMgmt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ContactMgmt")]
+[assembly: AssemblyTitle("Contact Management")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContactMgmt")]
+[assembly: AssemblyProduct("Contact Management")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
